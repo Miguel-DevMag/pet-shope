f198875544ddd9cd59d76f6c20e3edace666e97c
Miguel <mangellaura006@gmail.com>
2026-03-12 22:41:14 -0300

    Criando a pagina inicial do sistema, inserindo imagens dos produtos da nosso lota de veterenarios e adoção de animais.

--- app/src/main/java/com/example/pet_shop/InicialActivity.java
+++ app/src/main/java/com/example/pet_shop/InicialActivity.java
@@ -4,14 +4,17 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-public class OrderFulfilledActivity extends AppCompatActivity {
+public class InicialActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_order_completed);
+        setContentView(R.layout.activity_inicial);
 
     }
 }
